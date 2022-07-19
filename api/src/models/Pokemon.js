3c63f8ec70c9b19a091bd408edc8da02374c51b9
mocho66 <radrianquintana@gmail.com>
2022-07-19 16:46:40 -0300

    BD new

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,10 +3,51 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define('Pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      // autoIncrement: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
+    },
+    // DataTypes.INTEGER son valores entre -2,147,483,647 y 2,147,483,647 para validar luego el valor
+    life: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: "https://www.pngmart.com/files/12/Pokemon-Ash-Ketchum-PNG-Free-Download.png"
+      // imagen de pokebola "https://imagenpng.com/wp-content/uploads/2016/09/Pokebola-pokeball-png-0.png"
+    },
+    creat: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
     },
   });
 };
